8d4def7c80d7ec5f0ebabc445274c899fe19e296
Frank Rastelli <frankice8910@gmail.com>
2025-09-12 18:33:08 -0400

    Add logic for playing round and playing game

--- javascript.js
+++ javascript.js
@@ -1,13 +1,14 @@
-// gets a random integer between min and max (min inclusive, max exclusive)
-function getRandomInt(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
-}
+let humanScore = 0;
+let computerScore = 0;
 
-// takes the value from getRandomInt and uses it to make a choice at random
-function getComputerChoice(int) {
+// gets a random int and uses it to make a selection
+function getComputerChoice() {
     let computerChoice;
+
+    const minCeiled = Math.ceil(0);
+    const maxFloored = Math.floor(3);
+
+    let int = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 
     if (int == 0) {
         computerChoice = "Rock";
@@ -15,8 +16,7 @@ function getComputerChoice(int) {
     else if (int == 2) {
         computerChoice = "Scissors";
     }
-    else
-    {
+    else {
         computerChoice = "Paper";
     }
 
@@ -26,8 +26,63 @@ function getComputerChoice(int) {
 // takes user input for their choice
 function getHumanChoice() {
     let humanChoice = prompt("Choose Rock, Paper, or Scissors.")
-    return humanChoice;
+
+    // converts first letter of user input to uppercase and others to lowercase
+    return humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+;
 }
 
-console.log(getComputerChoice(getRandomInt(0,3)));
-console.log(getHumanChoice());
+// logic for playing a round
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice == "Rock" && computerChoice == "Scissors") {
+        console.log("You win! Rock beats scissors.");
+        humanScore++;
+    }
+    else if (humanChoice == "Paper" && computerChoice == "Rock") {
+        console.log("You win! Paper beats rock.");
+        humanScore++;
+    }
+    else if (humanChoice == "Scissors" && computerChoice == "Paper") {
+        console.log("You win! Scissors beats paper.");
+        humanScore++;
+    }
+    else if (humanChoice == computerChoice) {
+        console.log("It's a tie!")
+    }
+    else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    }
+}
+
+function playGame() {
+        let roundNumber = 1;
+
+    for (i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        console.log(`Round ${roundNumber}:`)
+
+        console.log(`You picked ${humanSelection}`);
+        console.log(`Computer picked ${computerSelection}`);
+
+        playRound(humanSelection, computerSelection);
+
+        console.log(`Your score: ${humanScore}`);
+        console.log(`Computer score: ${computerScore}`);
+
+        roundNumber++;
+    }
+
+    if (humanScore > computerScore) {
+        console.log("You win!");
+    }
+    else {
+        console.log("You lose!");
+    }
+
+}
+
+playGame();
